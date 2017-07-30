5db42b8b9cf5127f2f5e3fa40d3de16338c90081
Leo Koo <leokoo@gmail.com>
2017-07-30 18:28:02 +0800

    a basic controller example

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,16 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', 'TasksController@home');
+
+Route::get ( 'about' , function () { 
+	return view ( 'about' ); 
+}); 
+
+Route::get ( 'products' , function () { 
+	return view ( 'products' ); 
+});
+
+Route::get('services', function() {
+	return view ('services');
 });
